f1e622a16b388b2d8f5dc7b146dd2c5bd8bc105e
LLTae2 <taehyun5820@gmail.com>
2023-06-13 00:13:13 +0900

    Init::프로젝트 생성 후 샘플 코드 작성

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TodoListScreen(),
+    );
+  }
+}
+
+class TodoListScreen extends StatelessWidget {
+  final List<String> todoList = [
+    '할일 1',
+    '할일 2',
+    '할일 3',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo List'),
+      ),
+      body: ListView.builder(
+        itemCount: todoList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(todoList[index]),
+          );
+        },
+      ),
+    );
+  }
+}

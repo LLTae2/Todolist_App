dcb9543d8ebe5b180046cfb6ed0cf47e8952076c
LLTae2 <taehyun5820@gmail.com>
2023-06-13 19:24:54 +0900

    Update::디자인 및 수정 기능 추가

--- lib/todo_list_screen.dart
+++ lib/todo_list_screen.dart
@@ -69,6 +69,62 @@ class _TodoListScreenState extends State<TodoListScreen> {
     });
   }
 
+  void _editTodoItem(int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        final TextEditingController _editTitleController =
+            TextEditingController(text: todoList[index].title);
+        final TextEditingController _editDescriptionController =
+            TextEditingController(text: todoList[index].description);
+
+        return AlertDialog(
+          title: const Text('Edit Todo'),
+          content: Column(
+            children: [
+              TextField(
+                controller: _editTitleController,
+                decoration: const InputDecoration(
+                  labelText: 'Title',
+                ),
+                autofocus: true,
+              ),
+              TextField(
+                controller: _editDescriptionController,
+                decoration: const InputDecoration(
+                  labelText: 'Description',
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Save'),
+              onPressed: () {
+                final String editedTitle = _editTitleController.text;
+                final String editedDescription =
+                    _editDescriptionController.text;
+
+                setState(() {
+                  todoList[index].title = editedTitle;
+                  todoList[index].description = editedDescription;
+                });
+
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -76,31 +132,62 @@ class _TodoListScreenState extends State<TodoListScreen> {
         title: const Text('Todo List'),
       ),
       body: ListView.builder(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
         itemCount: todoList.length,
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(todoList[index].title),
-            onTap: () {
-              _showTodoDetails(todoList[index]);
-            },
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.delete),
-                  onPressed: () {
-                    _deleteTodoItem(index);
-                  },
-                ),
-                Checkbox(
-                  value: todoList[index].completed,
-                  onChanged: (value) {
-                    setState(() {
-                      todoList[index].completed = value!;
-                    });
-                  },
+          return Container(
+            margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 2,
+                  blurRadius: 5,
+                  offset: Offset(0, 3),
                 ),
               ],
+            ),
+            child: ListTile(
+              minLeadingWidth: 80,
+              title: Text(
+                todoList[index].title,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  decoration: todoList[index].completed
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none,
+                ),
+              ),
+              onTap: () {
+                _showTodoDetails(todoList[index]);
+              },
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Checkbox(
+                    value: todoList[index].completed,
+                    onChanged: (value) {
+                      setState(() {
+                        todoList[index].completed = value!;
+                      });
+                    },
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.edit),
+                    onPressed: () {
+                      _editTodoItem(index);
+                    },
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () {
+                      _deleteTodoItem(index);
+                    },
+                  ),
+                ],
+              ),
             ),
           );
         },

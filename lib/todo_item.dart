ee4311a9e14aebd7648cd87eaee1ac2411b9944e
LLTae2 <taehyun5820@gmail.com>
2023-06-15 10:01:37 +0900

    네비게이션 바

--- lib/todo_item.dart
+++ lib/todo_item.dart
@@ -2,10 +2,12 @@ class TodoItem {
   String title;
   String description;
   bool completed;
+  int priority;
 
   TodoItem({
     required this.title,
     required this.description,
     required this.completed,
+    required this.priority,
   });
 }
--- lib/todo_list_screen.dart
+++ lib/todo_list_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'todo_item.dart';
 
+const int _pageIndexTodoList = 0;
+const int _pageIndexOtherScreen = 1;
+
 class TodoListScreen extends StatefulWidget {
   const TodoListScreen({Key? key}) : super(key: key);
 
@@ -9,24 +12,32 @@ class TodoListScreen extends StatefulWidget {
 }
 
 class _TodoListScreenState extends State<TodoListScreen> {
+  int _currentPageIndex = _pageIndexTodoList;
   List<TodoItem> todoList = [];
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _priorityController = TextEditingController();
 
   void _addTodo() {
     final String newTitle = _titleController.text;
     final String newDescription = _descriptionController.text;
+    final int newPriority = int.tryParse(_priorityController.text) ?? 0;
+
     if (newTitle.isNotEmpty) {
       setState(() {
         todoList.add(TodoItem(
           title: newTitle,
           description: newDescription,
           completed: false,
+          priority: newPriority,
         ));
       });
+
       _titleController.clear();
       _descriptionController.clear();
+      _priorityController.clear();
     }
+
     Navigator.of(context).pop();
   }
 
@@ -43,8 +54,15 @@ class _TodoListScreenState extends State<TodoListScreen> {
                 const SizedBox(height: 5.0),
                 Text(
                   todoItem.description,
-                  style: TextStyle(
-                    color: Colors.grey, // 적용할 색상 설정
+                  style: const TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+                const SizedBox(height: 5.0),
+                Text(
+                  'Priority: ${todoItem.priority}',
+                  style: const TextStyle(
+                    color: Colors.grey,
                   ),
                 ),
               ],
@@ -57,26 +75,37 @@ class _TodoListScreenState extends State<TodoListScreen> {
                 Navigator.of(context).pop();
               },
             ),
+            TextButton(
+              child: const Text('Edit'),
+              onPressed: () {
+                _editTodoItem(todoItem);
+              },
+            ),
+            TextButton(
+              child: const Text('Delete'),
+              onPressed: () {
+                _deleteTodoItem(todoItem);
+                Navigator.of(context).pop();
+              },
+            ),
           ],
         );
       },
     );
   }
 
-  void _deleteTodoItem(int index) {
-    setState(() {
-      todoList.removeAt(index);
-    });
-  }
+  void _editTodoItem(TodoItem todoItem) {
+    final int index = todoList.indexOf(todoItem); // 수정할 Todo 항목의 인덱스
 
-  void _editTodoItem(int index) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         final TextEditingController _editTitleController =
-            TextEditingController(text: todoList[index].title);
+            TextEditingController(text: todoItem.title);
         final TextEditingController _editDescriptionController =
-            TextEditingController(text: todoList[index].description);
+            TextEditingController(text: todoItem.description);
+        final TextEditingController _editPriorityController =
+            TextEditingController(text: todoItem.priority.toString());
 
         return AlertDialog(
           title: const Text('Edit Todo'),
@@ -95,6 +124,13 @@ class _TodoListScreenState extends State<TodoListScreen> {
                   labelText: 'Description',
                 ),
               ),
+              TextField(
+                controller: _editPriorityController,
+                decoration: const InputDecoration(
+                  labelText: 'Priority',
+                ),
+                keyboardType: TextInputType.number,
+              ),
             ],
           ),
           actions: [
@@ -110,12 +146,16 @@ class _TodoListScreenState extends State<TodoListScreen> {
                 final String editedTitle = _editTitleController.text;
                 final String editedDescription =
                     _editDescriptionController.text;
+                final int editedPriority =
+                    int.tryParse(_editPriorityController.text) ?? 0;
 
                 setState(() {
                   todoList[index].title = editedTitle;
                   todoList[index].description = editedDescription;
+                  todoList[index].priority = editedPriority;
                 });
 
+                Navigator.of(context).pop();
                 Navigator.of(context).pop();
               },
             ),
@@ -125,72 +165,116 @@ class _TodoListScreenState extends State<TodoListScreen> {
     );
   }
 
+  void _deleteTodoItem(TodoItem todoItem) {
+    setState(() {
+      todoList.remove(todoItem);
+    });
+  }
+
+  MaterialColor setColor(int priority) {
+    if (priority <= 2) {
+      return Colors.red;
+    } else if (priority <= 4) {
+      return Colors.orange;
+    } else if (priority <= 6) {
+      return Colors.yellow;
+    } else if (priority <= 8) {
+      return Colors.green;
+    } else {
+      return Colors.blue;
+    }
+  }
+
+  List<TodoItem> _sortTodoListByPriority() {
+    // 우선순위에 따라 Todo 항목 정렬
+    todoList.sort((a, b) => a.priority.compareTo(b.priority));
+
+    return todoList;
+  }
+
+  Widget _buildTodoListScreen() {
+    return Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
+            itemCount: _sortTodoListByPriority().length,
+            itemBuilder: (context, index) {
+              final sortedTodoList = _sortTodoListByPriority();
+              final todoItem = sortedTodoList[index];
+
+              return Container(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.2),
+                      spreadRadius: 3,
+                      blurRadius: 1,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: ListTile(
+                  minLeadingWidth: 80,
+                  leading: Container(
+                    width: 24.0,
+                    height: 24.0,
+                    decoration: BoxDecoration(
+                      color: setColor(todoItem.priority),
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                  title: Text(
+                    todoItem.title,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      decoration: todoItem.completed
+                          ? TextDecoration.lineThrough
+                          : TextDecoration.none,
+                    ),
+                  ),
+                  onTap: () {
+                    _showTodoDetails(todoItem);
+                  },
+                  trailing: Checkbox(
+                    value: todoItem.completed,
+                    onChanged: (value) {
+                      setState(() {
+                        todoItem.completed = value!;
+                      });
+                    },
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildOtherScreen() {
+    return Center(
+      child: Text('Other Screen'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Todo List'),
+        title: Text('Todo List'),
       ),
-      body: ListView.builder(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
-        itemCount: todoList.length,
-        itemBuilder: (context, index) {
-          return Container(
-            margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10.0),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: 2,
-                  blurRadius: 5,
-                  offset: Offset(0, 3),
-                ),
-              ],
-            ),
-            child: ListTile(
-              minLeadingWidth: 80,
-              title: Text(
-                todoList[index].title,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  decoration: todoList[index].completed
-                      ? TextDecoration.lineThrough
-                      : TextDecoration.none,
-                ),
-              ),
-              onTap: () {
-                _showTodoDetails(todoList[index]);
-              },
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Checkbox(
-                    value: todoList[index].completed,
-                    onChanged: (value) {
-                      setState(() {
-                        todoList[index].completed = value!;
-                      });
-                    },
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.edit),
-                    onPressed: () {
-                      _editTodoItem(index);
-                    },
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.delete),
-                    onPressed: () {
-                      _deleteTodoItem(index);
-                    },
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
+      body: IndexedStack(
+        index: _currentPageIndex,
+        children: [
+          _buildTodoListScreen(),
+          _buildOtherScreen(),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
@@ -215,6 +299,13 @@ class _TodoListScreenState extends State<TodoListScreen> {
                         labelText: 'Description',
                       ),
                     ),
+                    TextField(
+                      controller: _priorityController,
+                      decoration: const InputDecoration(
+                        labelText: 'Priority',
+                      ),
+                      keyboardType: TextInputType.number,
+                    ),
                   ],
                 ),
                 actions: [
@@ -233,6 +324,24 @@ class _TodoListScreenState extends State<TodoListScreen> {
             },
           );
         },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentPageIndex,
+        onTap: (index) {
+          setState(() {
+            _currentPageIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Todo List',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            label: 'Other Screen',
+          ),
+        ],
       ),
     );
   }
